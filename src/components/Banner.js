bf8cc2130e835f450a56b8e0c052a6eadbf2f690
Erwan-Cloitre <erwan.cloitre@gmail.com>
2022-09-12 21:53:40 +0200

    Skills&ProjectsCSS

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -46,7 +46,7 @@ const tick = () => {
           <Row>
             <Col xs={12} md={6} xl={7}>
               <span className="tagline">Welcome to my Portfolio</span>
-              <h1>{`Hi I'm Erwan Cloitre `}<span className="wrap">{text}</span></h1>
+              <h1>{`Hi I'm Erwan Cloitre `}<br/><span className="wrap">{text}</span></h1>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
               <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
             </Col>
